1742fe1cf36c81b8e5ada204de508c7b2f278ec6
richardazevedo <richardazevedo@me.com>
2020-10-17 10:01:34 -0300

    09_front_x.py

--- 09_front_x.py
+++ 09_front_x.py
@@ -13,8 +13,13 @@ antes de combina-las.
 
 def front_x(words):
     # +++ SUA SOLUÇÃO +++
-    return
 
+    words.sort(key=func_x)
+    return words
+
+
+def func_x(x):
+    return x if x[0] != 'x' else x.replace('x', ' ')
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
 
